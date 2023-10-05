5c8905a5a25cc6a4a8ec0be04e6604c739fbbe42
Juanj Sanchez <juanjosesanchezgil@gmail.com>
2023-10-05 15:24:22 +0200

    añadiendo tags y antes de static

--- routers/products.py
+++ routers/products.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/products")
+router = APIRouter(prefix="/products", tags=["products"], responses={404 : {"message" : "No encontrado"}})
 
 products_list = ["Producto 1", "Producto 2", "Producto 3", "Producto 4", "Producto 5"]
 
